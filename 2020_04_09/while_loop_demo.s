9e0a7b66d65c30b44d84e3eb8e095e636d348462
Paul Conrad <pjc@pauljconrad.net>
2020-04-09 20:07:37 -0700

    Update of while_loop_demo.s from lab

--- 2020_04_09/while_loop_demo.s
+++ 2020_04_09/while_loop_demo.s
@@ -20,7 +20,7 @@ value_read: .word 0
 .text
 .global main
 main:
-	push {lr}		/* save our return address */
+	push {lr} /* save our return address */
 
 	// register r4 holds address to value_read
 	ldr r4, =value_read
@@ -39,11 +39,25 @@ next:
 	// loop_counter is register r0
 	//
 	// r5=0;
+	// r0=1;
 	// while ( r0 < r4 )
 	// {
 	//	r5 = r5 + r0;
 	//	r0++;
 	// }
+	ldr r4, [r4]		// take the data at the address in r4, and store it in r4
+	mov r5, #0		// r5=0;
+	mov r0, #1		// r0=1; r0 is our loop counter variable
+while_r0_lt_r4:			// while ( r0 < r4 ) {
+	cmp r0, r4		// q: what are we comparing? a: r0 and r4
+	bge end_while_r0_lt_r4	// condition code is inverse of less than, because if comparison is false, break out loop
+
+	// code for inside the loop body
+	add r5, r5, r0		// ARM: add r5, r0 ( r5+=r0; ) is the same thing as well
+	add r0, r0, #1		// ARM: add r0, #1 ( r0+=1; )
+
+	bal while_r0_lt_r4	// branch back to start of while loop ( "GOTO while_r0_lt_r4" )
+end_while_r0_lt_r4:		// }
 
 	ldr r0, =response	/* r0 contains pointer to response message */
 	mov r1, r4		/* r1 contains pointer to value_read */
